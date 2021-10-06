7776f7c85351ebf247236a71db27cce264208c28
asmi046 <asmi046@gmail.com>
2021-10-07 00:09:13 +0300

    Тег яндекса

--- header.php
+++ header.php
@@ -7,6 +7,9 @@
   <meta charset="UTF-8">
   <meta name="format-detection" content="telephone=no">
   <meta name="description" content="Новый сайт">
+
+  <meta name="yandex-verification" content="96cc21ba5e2d2303" />
+
   <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri();?>/img/falt/icon256.png" sizes="256x256">
   <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri();?>/img/falt/icon128.png" sizes="128x128">
   <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri();?>/img/falt/icon64.png" sizes="64x64">
