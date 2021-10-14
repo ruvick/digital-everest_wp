d5642d80c3d89c862d834640f1f31fc820f9d99c
asmi046 <asmi046@gmail.com>
2021-10-15 00:46:35 +0300

    ВИджет

--- header.php
+++ header.php
@@ -53,6 +53,13 @@
     });
   </script>
 
+  <script>
+          (function(w,d,u){
+                  var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                  var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+          })(window,document,'https://cdn-ru.bitrix24.ru/b12842324/crm/site_button/loader_8_6yhw9r.js');
+  </script>
+
   <?php wp_head();?>   
 
 </head>
